c4723e9c89c01893c2279a0947d6169adbc4f0bd
testgithubritik <161156122+testgithubritik@users.noreply.github.com>
2024-10-18 18:40:52 +0530

    assignment

---
+++ index.js
@@ -0,0 +1,91 @@
+//1
+
+/*let num1 = 2;
+let num2 = 5;
+let sum =  (num1+num2);
+console.log (sum);*/
+
+
+//2
+
+/*let a = 5;
+let b = 3;
+ let  sum = (a,b);
+if (a > 0 && b > 0) {
+   console.log("both number are possitive.");
+} else if (a < 0 && b < 0){
+   console.log("both number are negative.");
+} else if (a === 0 || b === 0){
+   console.log("at least one number is zero.");
+}else{
+   console.log("conditions are not met.");
+}*/
+
+
+//4
+
+/*let num =4567;
+let firstdigit =(num);
+ console.log ("the first digit is:4")*/
+
+
+ //5
+
+/* let num = 4567;
+ let lastdigit =(num);
+ console.log ("the last digit is:7") */
+
+
+ //6
+
+/*let dividend = 9;
+let divisor  = 2;
+let remainder = dividend % divisor;
+console.log(remainder)*/
+
+//7
+
+/*let a = 5;
+let b = 2;
+let number =(a*b);
+console.log(number)*/
+
+
+//8
+
+
+/*let a = 50;
+let b = 20;
+let c = 100;
+let total =calculatesum(a,b,c);
+let average = calculateaverage(a,b,c);
+function calculatesum(a, b, c){return a + b + c;}
+function calculateaverage(a,b,c){let sum = calculatesum(a,b,c); return sum /3;}
+console.log("total marks; " + total);
+console.log("average marks: " + average);*/
+
+//3
+
+
+let a = 12;
+let b = 20;
+function conditions (a,b) {
+   if (a % 10 === 0 && b % 10 === 0){
+      console.log("both are divisible by 10");
+   }
+   else if( a % 10 !== 0 && b % 10 !== 0){
+      console.log("both are not divisible ");
+   }
+   else if (a % 10 === 0 || b % 10 === 0){
+      console.log("one of them is divisible by 10");
+   }
+}
+   
+
+
+
+
+
+
+
+
